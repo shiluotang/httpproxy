14f12152d1f91fb792236a68ccb444b31836ed67
Samuel Winchester <samuel@debian>
2014-05-24 23:13:44 +0800

    Use dummy implementation just for compilation, we will later implement it for mingw.

--- src/win32/thread.cpp
+++ src/win32/thread.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 namespace httpproxy {
 
+	thread::id::id(native_handle_type handle) {}
+
 	struct thread::id::impl { };
 
 	void swap(thread &a, thread &b) noexcept { }
@@ -13,7 +15,7 @@ namespace httpproxy {
 	thread::thread() noexcept {}
 	thread::~thread() { }
 
-	thread::id thread::get_id() const noexcept { return id(); }
+	thread::id thread::get_id() const noexcept { return id(-1L); }
 	thread::native_handle_type thread::native_handle() { return 0L; }
 	void thread::detach() {}
 	void thread::join() {}
