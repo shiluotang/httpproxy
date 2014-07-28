f2a84d0057a0a6276ccdeb4574d6035ab65bf908
Quangang Sheng <shengquangang2008@gmail.com>
2014-07-28 18:21:41 +0800

    refactor field name

--- src/win32/socket.cpp
+++ src/win32/socket.cpp
@@ -20,41 +20,41 @@ using namespace std;
 
 namespace httpproxy {
 
-    socket::socket() :m_handle_ptr(new socket_handle()) {}
-    socket::socket(int handle) :m_handle_ptr(new socket_handle(handle)) {}
+    socket::socket() :_M_handle_ptr(new socket_handle()) {}
+    socket::socket(int handle) :_M_handle_ptr(new socket_handle(handle)) {}
     socket::socket(int address_family, int socket_type, int protocol_type)
-        :m_handle_ptr(new socket_handle(address_family, socket_type, protocol_type))
+        :_M_handle_ptr(new socket_handle(address_family, socket_type, protocol_type))
     {}
     socket::~socket() {}
 
     int socket::send(void const* buf, size_t num) {
-        return ::send(static_cast<SOCKET>(m_handle_ptr->_M_data),
+        return ::send(static_cast<SOCKET>(_M_handle_ptr->_M_data),
                 reinterpret_cast<char const*>(buf), num,
                 0);
     }
     int socket::receive(void *buf, size_t buf_size) {
-        return ::recv(static_cast<SOCKET>(m_handle_ptr->_M_data),
+        return ::recv(static_cast<SOCKET>(_M_handle_ptr->_M_data),
                 reinterpret_cast<char*>(buf), buf_size,
                 0);
     }
 
-    socket::operator bool() const { return m_handle_ptr->operator bool(); }
+    socket::operator bool() const { return _M_handle_ptr->operator bool(); }
 
     socket socket::accept() {
         sockaddr addr = {0};
         int addrlen = sizeof(addr);
-        SOCKET s = ::accept(static_cast<SOCKET>(m_handle_ptr->_M_data), 
+        SOCKET s = ::accept(static_cast<SOCKET>(_M_handle_ptr->_M_data), 
                 &addr, &addrlen);
         clog << "received connection from " << inet_address(&addr, addrlen) << endl;
         return socket(s);
     }
 
     bool socket::bind(inet_address const& addr) {
-        return ::bind(static_cast<SOCKET>(m_handle_ptr->_M_data),
+        return ::bind(static_cast<SOCKET>(_M_handle_ptr->_M_data),
                 reinterpret_cast<sockaddr const*>(addr.raw_addr()),
                 addr.raw_addr_len()) == 0;
     }
     bool socket::listen(int backlog) {
-        return ::listen(static_cast<SOCKET>(m_handle_ptr->_M_data), backlog) == 0;
+        return ::listen(static_cast<SOCKET>(_M_handle_ptr->_M_data), backlog) == 0;
     }
 }
