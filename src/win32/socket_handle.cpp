aed6addd24084a732ce4a0693f9336c011129b47
Quangang Sheng <shengquangang2008@gmail.com>
2014-07-28 18:22:22 +0800

    refactor field name

--- src/win32/socket_handle.cpp
+++ src/win32/socket_handle.cpp
@@ -9,30 +9,30 @@ using namespace std;
 
 namespace httpproxy {
 
-    socket_handle::socket_handle() :m_data(INVALID_SOCKET) {
-        m_data = ::socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    socket_handle::socket_handle() :_M_data(INVALID_SOCKET) {
+        _M_data = ::socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     }
 
-    socket_handle::socket_handle(int handle) :m_data(handle) {}
+    socket_handle::socket_handle(int handle) :_M_data(handle) {}
     socket_handle::socket_handle(int address_family, int socket_type,
-            int protocol_type) :m_data(INVALID_SOCKET) {
-        m_data = ::socket(address_family, socket_type, protocol_type);
+            int protocol_type) :_M_data(INVALID_SOCKET) {
+        _M_data = ::socket(address_family, socket_type, protocol_type);
     }
     socket_handle::~socket_handle() { close(); }
 
 
     void socket_handle::close() {
-        if(m_data != INVALID_SOCKET) {
+        if(_M_data != INVALID_SOCKET) {
             int retcode;
-            retcode = ::shutdown(static_cast<SOCKET>(m_data), SD_BOTH);
+            retcode = ::shutdown(static_cast<SOCKET>(_M_data), SD_BOTH);
             clog << "shutdown retcode is " << retcode << endl;
-            retcode = ::closesocket(static_cast<SOCKET>(m_data));
+            retcode = ::closesocket(static_cast<SOCKET>(_M_data));
             clog << "closesocket retcode is " << retcode << endl;
             if(retcode == 0)
-                m_data = INVALID_SOCKET;
+                _M_data = INVALID_SOCKET;
         }
     }
 
-    socket_handle::operator bool() const { return m_data != INVALID_SOCKET; }
+    socket_handle::operator bool() const { return _M_data != INVALID_SOCKET; }
 
 }
