3b99dba968215da02bb1fec37a2137dffe93084c
Quangang Sheng <shengquangang2008@gmail.com>
2014-08-04 14:18:13 +0800

    stop_watch

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,6 +2,7 @@
 
 #include <cstdlib>
 #include <string>
+#include <ostream>
 
 #include <cxxabi.h>
 
@@ -18,5 +19,40 @@ namespace httpproxy {
         free(s);
         return origin_name;
     }
+
+	stop_watch::stop_watch()
+		:_M_start(time_point::min()),
+		_M_duration(duration::zero()),
+		_M_running(false) {}
+
+	void stop_watch::start() {
+		if (_M_running)
+			return;
+		_M_start = clock_type::now();
+		_M_running = true;
+	}
+	
+	void stop_watch::stop() {
+		if (!_M_running)
+			return;
+		_M_duration += clock_type::now() - _M_start;
+		_M_running = false;
+	}
+
+	void stop_watch::reset() {
+		_M_start = time_point::min();
+		_M_duration = duration::zero();
+		_M_running = false;
+	}
+
+	double stop_watch::elapsedMilliseconds() const {
+		if (!_M_running)
+			return chrono::duration_cast<double_milli>(_M_duration).count();
+		return chrono::duration_cast<double_milli>((clock_type::now() - _M_start) + _M_duration).count();
+	}
+
+	ostream& operator << (ostream &os, stop_watch const &watch) {
+		return os << watch.elapsedMilliseconds() << " ms";
+	}
 
 }
