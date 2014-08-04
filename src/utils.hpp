570b93414fae615d8cfa8660f6ff8b2dace65065
Quangang Sheng <shengquangang2008@gmail.com>
2014-08-04 14:17:59 +0800

    stop_watch

--- src/utils.hpp
+++ src/utils.hpp
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <sstream>
 #include <iosfwd>
+#include <chrono>
 
 namespace httpproxy {
 
@@ -19,6 +20,25 @@ namespace httpproxy {
 
     extern std::string const demangle(std::string const& mangled_name);
 
+	class stop_watch {
+		protected:
+			typedef std::chrono::high_resolution_clock clock_type;
+			typedef clock_type::time_point time_point;
+			typedef clock_type::duration duration;
+			typedef std::chrono::duration<double, std::milli> double_milli;
+		public:
+			stop_watch();
+			void start();
+			void stop();
+			void reset();
+			double elapsedMilliseconds() const;
+		private:
+			time_point _M_start;
+			duration _M_duration;
+			bool _M_running;
+	};
+
+	std::ostream& operator << (std::ostream&, stop_watch const&);
 }
 #define DEMANGLE(X) ::httpproxy::demangle(typeid(X).name())
 
