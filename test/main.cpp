1dff91ed3d29f01a3962a0c280e52ba9e60777f1
shiluotang <shengquangang2008@gmail.com>
2015-04-13 02:46:39 +0800

    format source code with vim "retab" command.

--- test/main.cpp
+++ test/main.cpp
@@ -31,35 +31,35 @@ int test_socket(int argc, char* argv[]) {
 }
 
 int test_http(int argc, char* argv[]) {
-	httpproxy::socket_environment env;
-	vector<httpproxy::inet_address> addresses;
-	if (httpproxy::inet_address::lookup("www.baidu.com", "80", addresses) < 1)
-		return EXIT_FAILURE;
-	string const REQUEST = "GET http://www.baidu.com/ HTTP/1.1\r\n"
-		"Host: www.baidu.com\r\n"
-		"\r\n\r\n";
-	httpproxy::socket s(AF_INET, SOCK_STREAM, IPPROTO_IP);
-	s.connect(addresses[0]);
-	clog << "sending data..." << endl;
-	clog << "REQUEST is " << REQUEST << endl;
-	s.send(REQUEST.data(), REQUEST.size());
-	char buf[4096];
-	int cnt;
-	stringstream ss;
-	while ((cnt = s.receive(buf, sizeof(buf))) > 0) {
-		clog << "reading " << cnt << " bytes." << endl;
-		ss.write(buf, cnt);
-	}
-	cout << "received data is " << ss.str() << endl;
-	return EXIT_SUCCESS;
+    httpproxy::socket_environment env;
+    vector<httpproxy::inet_address> addresses;
+    if (httpproxy::inet_address::lookup("www.baidu.com", "80", addresses) < 1)
+        return EXIT_FAILURE;
+    string const REQUEST = "GET http://www.baidu.com/ HTTP/1.1\r\n"
+        "Host: www.baidu.com\r\n"
+        "\r\n\r\n";
+    httpproxy::socket s(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    s.connect(addresses[0]);
+    clog << "sending data..." << endl;
+    clog << "REQUEST is " << REQUEST << endl;
+    s.send(REQUEST.data(), REQUEST.size());
+    char buf[4096];
+    int cnt;
+    stringstream ss;
+    while ((cnt = s.receive(buf, sizeof(buf))) > 0) {
+        clog << "reading " << cnt << " bytes." << endl;
+        ss.write(buf, cnt);
+    }
+    cout << "received data is " << ss.str() << endl;
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char* argv[]) {
     try {
-		httpproxy::stop_watch watch;
-		watch.start();
+        httpproxy::stop_watch watch;
+        watch.start();
         int retcode = test_http(argc, argv);
-		watch.stop();
+        watch.stop();
         clog << "Running time is " << watch << "." << endl;
         return retcode;
     } catch(exception &e) {
